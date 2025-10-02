9aadfeca5801687cd49147e02a635c275d0879cd
Andrés Rojas <afrojas@binti.com>
2025-10-01 19:38:22 -0700

    sample first thing

---
+++ setup_for_laptop_development.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# -e tells bash to exit immediately on error, instead of its default of continuing
+# -x prints out each command being run for easier debugging
+# set -e
+
+echo "setup for laptop development"
